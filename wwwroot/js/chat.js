b16f237990f47ff2ac17c980727946b3529b7255
Chris Gomez <cagomez@cellbycell.com>
2020-06-15 16:11:27 -0400

    Using unpkg for signalr

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,30 +1,41 @@
 "use strict";
 
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    }
+    else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
 let connection = new signalR
     .HubConnectionBuilder()
     .withUrl("/chatHub")
     .build();
 
-document.getElementById('sendButton').disabled = true;
-
-connection.on('ReceiveMessage', function(user, message) {
+connection.on('ReceiveMessage', function (user, message) {
     const messageList = document.getElementById('messagesList')
-
     const newListItem = document.createElement('li');
+
     newListItem.appendChild(document.createTextNode(`${user}: ${message}`));
     messageList.appendChild(newListItem);
 });
 
-connection.start().then(function () {
-    document.getElementById('sendButton').disabled = false;
-    console.log("connection started");
-});
+ready(() => {
+    connection.start().then(function () {
+        document.getElementById('sendButton').disabled = false;
+        console.log("connection started");
+    });
 
-document.getElementById('sendButton').addEventListener('click', function (event) {
-    const user = document.getElementById('userInput').value;
-    const message = document.getElementById('messageInput').value;
+    document.getElementById('sendButton').addEventListener('click', function (event) {
+        const user = document.getElementById('userInput').value;
+        const message = document.getElementById('messageInput').value;
 
-    connection.invoke('SendMessage', user, message);
+        connection.invoke('SendMessage', user, message);
 
-    event.preventDefault();
+        event.preventDefault();
+    });
+
+    document.getElementById('sendButton').disabled = true;
 });
